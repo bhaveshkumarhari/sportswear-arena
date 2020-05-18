8ff093dc807167b8df6ea7ee17d654f71ba08b8a
bhaveshkumarhari <bhaveshkumar.hari@gmail.com>
2020-05-18 15:38:49 +0100

    Styling signup, signin and logout page

--- sportsweararena/settings.py
+++ sportsweararena/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
 
+    'crispy_forms',
+
     'core',
 ]
 
@@ -149,3 +151,8 @@ AUTHENTICATION_BACKENDS = (
 
 SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
+
+
+# CRISPY FORMS
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
